e63862710dc04f6c0f8a1e1b262487cf785c1174
Marcel Joschko <marceljoschko@protonmail.com>
2022-09-28 09:58:27 +0200

    working

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,12 @@
 // get all elements
 
 const startContext = document.getElementById("startContext");
-const obama = document.getElementById("obama");
-const overlay = document.getElementById("overlay");
 
 const container = document.getElementById("particleContainer");
 const canvas = document.createElement("canvas");
+
+// every 10ms
+const TIME_TIL_MAX_VOL = 3000;
 
 const playPause = document.getElementById("playPause");
 
@@ -16,20 +17,25 @@ let musicPlaying = false;
 const samples = [];
 const NUMBER_OF_SAMPLES = 59;
 
-let ROWS = 200;
-let COLS = 400;
+let imgWidth = 250;
+let imgHeight = 146;
+
+let ROWS = imgHeight;
+let COLS = imgWidth;
+
+const audioFiles = {};
+
+let img = new Image();
 
 let NUM_PARTICLES = ROWS * COLS,
     THICKNESS = Math.pow(80, 2),
-    SPACING = 3,
-    MARGIN = 10,
+    SPACING = 4,
+    MARGIN = 500,
     COLOR = 220,
-    DRAG = 0.99,
-    EASE = 0.25,
+    DRAG = 0.995,
+    EASE = 0.05,
     particle,
     bounds,
-    mouse,
-    stats,
     list,
     ctx,
     tog,
@@ -42,14 +48,10 @@ let NUM_PARTICLES = ROWS * COLS,
     f,
     a,
     b,
-    i,
     n,
     w,
     h,
-    p,
-    s,
-    r,
-    c;
+    p;
 
 particle = {
     vx: 0,
@@ -58,11 +60,109 @@ particle = {
     y: 0,
 };
 
+let channelMerger, reverb;
+let audioCtx;
+let bufferLoader;
+
+let gain, source1, panner, listener;
+let sammpleSource;
+
+let bufferArray;
+
+list = [];
+let particleMap = [];
+
+const loadImage = () => {
+    particleInit();
+
+    img.onload = function () {
+        let cvs = document.createElement("canvas");
+        let cvsCtx = cvs.getContext("2d");
+
+        cvs.width = imgWidth;
+        cvs.height = imgHeight;
+
+        cvsCtx.drawImage(img, 0, 0);
+        for (let i = 0; i < COLS; i++) {
+            particleMap[i] = [];
+            for (let j = 0; j < ROWS; j++) {
+                let tempImg = cvsCtx.getImageData(i, j, 1, 1);
+
+                if (tempImg.data[3] > 0) {
+                    p = Object.create(particle);
+                    p.x = p.ox = MARGIN + SPACING * i;
+                    p.y = p.oy = MARGIN + SPACING * j;
+
+                    particleMap[i][j] = p;
+                } else {
+                    particleMap[i][j] = 0;
+                }
+            }
+        }
+
+        list = [].concat(...particleMap);
+
+        //cvs.remove();
+
+        particleStep();
+        //loadSamples();
+    };
+    img.src = "/images/venice_small.png";
+};
+
+const setupAudio = () => {
+    if (!audioCtx) {
+        try {
+            audioCtx = new AudioContext();
+
+            bufferLoader = new BufferLoader(
+                audioCtx,
+                [
+                    "../audio/atmo.mp3",
+                    "../audio/IMreverbs/Large_Long_Echo_Hall.wav",
+                    "../audio/pianokey.mp3",
+                ],
+                finishedLoading
+            );
+
+            let parentNode = startContext.parentElement;
+            parentNode.remove();
+
+            playPause.style.display = "block";
+
+            // route merger to output
+
+            console.log("created audiocontext");
+
+            bufferLoader.load();
+        } catch (e) {
+            alert(e);
+        }
+    }
+};
+
+function finishedLoading(bufferList) {
+    bufferArray = bufferList;
+    // Create two sources and play them both together.
+    source1 = audioCtx.createBufferSource();
+    gain = audioCtx.createGain();
+
+    source1.buffer = bufferList[0];
+
+    reverb = audioCtx.createConvolver();
+    panner = audioCtx.createStereoPanner();
+
+    reverb.buffer = bufferList[1];
+
+    source1.connect(gain).connect(audioCtx.destination);
+
+    source1.loop = true;
+    source1.start(0, Math.floor(Math.random() * 511));
+}
+
 const particleInit = () => {
     ctx = canvas.getContext("2d");
     tog = true;
-
-    list = [];
 
     w = canvas.width = COLS * SPACING + MARGIN * 2;
     h = canvas.height = ROWS * SPACING + MARGIN * 2;
@@ -70,18 +170,19 @@ const particleInit = () => {
     container.style.marginLeft = Math.round(w * -0.5) + "px";
     container.style.marginTop = Math.round(h * -0.5) + "px";
 
-    for (i = 0; i < NUM_PARTICLES; i++) {
-        p = Object.create(particle);
-        p.x = p.ox = MARGIN + SPACING * (i % COLS);
-        p.y = p.oy = MARGIN + SPACING * Math.floor(i / COLS);
+    // for (i = 0; i < NUM_PARTICLES; i++) {
+    //     p = Object.create(particle);
+    //     p.x = p.ox = MARGIN + SPACING * (i % COLS);
+    //     p.y = p.oy = MARGIN + SPACING * Math.floor(i / COLS);
 
-        list[i] = p;
-    }
+    //     list[i] = p;
+    // }
 
     container.addEventListener("mousemove", function (e) {
         bounds = container.getBoundingClientRect();
         mx = e.clientX - bounds.left;
         my = e.clientY - bounds.top;
+        if (panner) panner.pan.value = ((e.clientX - w) / w) * -1;
     });
 
     canvas.addEventListener("click", playSound);
@@ -91,12 +192,11 @@ const particleInit = () => {
 
 const particleStep = () => {
     if ((tog = !tog)) {
-        for (i = 0; i < NUM_PARTICLES; i++) {
+        for (let i = 0; i < NUM_PARTICLES; i++) {
+            if (!list[i]) continue;
             p = list[i];
-
             d = (dx = mx - p.x) * dx + (dy = my - p.y) * dy;
             f = -THICKNESS / d;
-
             if (d < THICKNESS) {
                 t = Math.atan2(dy, dx);
                 p.vx += f * Math.cos(t);
@@ -109,7 +209,8 @@ const particleStep = () => {
     } else {
         b = (a = ctx.createImageData(w, h)).data;
 
-        for (i = 0; i < NUM_PARTICLES; i++) {
+        for (let i = 0; i < NUM_PARTICLES; i++) {
+            if (!list[i]) continue;
             p = list[i];
             (b[(n = (~~p.x + ~~p.y * w) * 4)] = b[n + 1] = b[n + 2] = COLOR),
                 (b[n + 3] = 255);
@@ -121,100 +222,37 @@ const particleStep = () => {
     requestAnimationFrame(particleStep);
 };
 
-const loadSamples = () => {
-    for (let i = 1; i <= NUMBER_OF_SAMPLES; i++) {
-        samples.push(
-            new Wad({
-                source: "../audio/samples/sample_" + i + ".wav",
-                delay: {},
-                panning: 0,
-                panningModel: "HRTF",
-                rolloffFactor: 1,
-            })
-        );
-    }
-};
-
 // variables
-
-let audioCtx;
 
 // functions
 
 const init = () => {
-    if (!audioCtx) {
-        try {
-            audioCtx = new AudioContext();
+    setupAudio();
 
-            let parentNode = startContext.parentElement;
-            parentNode.remove();
-
-            playPause.style.display = "block";
-
-            particleInit();
-            particleStep();
-
-            loadSamples();
-
-            loopify("../audio/ambient.wav", ready);
-
-            console.log("created audiocontext");
-        } catch (e) {
-            alert("Web Audio API is not supported in this browser");
-        }
-    }
+    loadImage();
 };
 
-const getRandomNumber = (max) => {
-    return Math.floor(Math.random() * max);
+const playSound = () => {
+    sammpleSource = audioCtx.createBufferSource();
+    sammpleSource.buffer = bufferArray[2];
+    sammpleSource.connect(reverb).connect(panner).connect(audioCtx.destination);
+    console.log(panner.pan);
+    sammpleSource.start();
 };
-
-const getRandomMinMax = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
-};
-
-const ready = (err, loop) => {
-    if (err) {
-        console.warn(err);
-    }
-
-    loop.play();
-
-    playPause.addEventListener("click", () => {
-        musicPlaying = !musicPlaying;
-
-        if (musicPlaying) loop.stop();
-        else loop.play();
-    });
-};
-
-const playSound = (e) => {
-    console.log(e.pageX, e.pageY);
-    let rnd = getRandomNumber(NUMBER_OF_SAMPLES);
-    let rndDelayTime = Math.random() * 2;
-    let rndFeedback = Math.random() * 1;
-    let rndPan = Math.random() * 2;
-
-    const parameters = {
-        delay: {
-            feedback: rndFeedback,
-            wet: 1,
-            delayTime: rndDelayTime,
-        },
-    };
-    //   panning: [
-    //     getRandomMinMax(-50, 50),
-    //     getRandomMinMax(-50, 50),
-    //     getRandomMinMax(-50, 50),
-    // ],
-
-    samples[rnd].play(parameters);
-};
-
-const playRandom = () => {};
 
 // wait till everything is loaded
 
 window.addEventListener("load", () => {
     startContext.addEventListener("click", init);
+    canvas.addEventListener("click", playSound);
+
+    playPause.addEventListener("click", () => {
+        musicPlaying = !musicPlaying;
+
+        if (audioCtx.state === "running") {
+            audioCtx.suspend();
+        } else if (audioCtx.state === "suspended") {
+            audioCtx.resume();
+        }
+    });
 });
